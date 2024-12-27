5b4fbdbab02f1e222861eb2f1a9e21e7ea687d9a
Eslam Radwan <eslamradwan145@gmail.com>
2024-12-27 03:04:19 +0200

    Finished 4/5 of work in landing page until features section

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -3,6 +3,9 @@ import Header from './Header';
 import About1 from './About1';
 import About2 from './About2';
 import WhatIsTotc from './WhatISTotc';
+import OurFeatures from './OurFeatures';
+import './landing.css'
+
 function Landing(props) {
     return (
         <main>
@@ -10,6 +13,7 @@ function Landing(props) {
             <About1/>
             <About2/>
             <WhatIsTotc/>
+            <OurFeatures />
         </main>
     );
 }
