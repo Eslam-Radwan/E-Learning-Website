9303b76eb1bb1fc808d17f2a0b0a2fe607e31b0a
Eslam Radwan <eslamradwan145@gmail.com>
2025-01-11 03:06:21 +0200

    Deploying 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/E-Learning-Website/',
   plugins: [react()],
+  base: '/E-Learning-Website/',
 })
