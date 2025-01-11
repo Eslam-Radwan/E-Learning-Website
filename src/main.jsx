250d25dfc35e506d45d9d73743f4af972f6dd794
Eslam Radwan <eslamradwan145@gmail.com>
2025-01-11 13:01:39 +0200

    edit paths 7

--- src/main.jsx
+++ src/main.jsx
@@ -6,23 +6,38 @@ import './index.css'
 import Landing from './components/LandingPage/Landing.jsx'
 import Login from './components/LoginPage/Login.jsx'
 import Blog from './components/BlogPage/Blog.jsx'
-import CourseDetail from './components/CourseDetailPage/CourseDetail.jsx'
-import Checkout from './components/CheckoutPage/Checkout.jsx'
-import BlogDetail from './components/BlogDetailPage/BlogDetail.jsx'
 import MemberShip from './components/MembershipPage/MemberShip.jsx'
 import Course from './components/CoursePage/Course.jsx'
 
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "landing",
+    element: <Landing />,
+  },
+  {
+    path: "courses",
+    element: <Course />
+  },
+  {
+    path: "blog",
+    element: <Blog />
+  },
+  {
+    path: "membership",
+    element: <MemberShip />
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/E-Learning-Website/"> 
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="/courses" element={<Course />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/membership" element={<MemberShip />} />
-      <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-    </Routes>
-  </BrowserRouter>
-);
+  <StrictMode>
+    <BrowserRouter basename='/E-Learning-Website/'>
+    <RouterProvider router={router} />
+    </BrowserRouter>
+  </StrictMode>,
+)
